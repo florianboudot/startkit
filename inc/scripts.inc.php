d3bff7fe52a16fc2aa73aa47a0832b1a0dba479e
kazes <supakazes@hotmail.com>
2014-03-19 22:38:46 +0100

    update jquery from 1.11.0 to 2.1.0

--- inc/scripts.inc.php
+++ inc/scripts.inc.php
@@ -1,5 +1,5 @@
 <!-- JQUERY javascript library -->
-<script src="js/jquery-1.11.0.min.js"></script>
+<script src="js/jquery-2.1.0.js"></script>
 
 <!-- all custom scripts in base.js -->
 <script src="js/base.js"></script>
